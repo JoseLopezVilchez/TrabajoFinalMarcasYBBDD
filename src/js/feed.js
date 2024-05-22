b90808ebd4d29ae284d050d85e954bfda1886f10
JoseLopezVilchez <jose.lopez@escuelaestech.es>
2024-05-22 14:24:11 +0200

    progreso del dia

---
+++ src/js/feed.js
@@ -0,0 +1,47 @@
+// --------------------
+// elementos referenciados
+//---------------------
+
+// drawer
+const formbusqueda = document.getElementById('formbusqueda')
+const inputbusqueda = document.getElementById('inputbusqueda')
+const botonbusqueda = document.getElementById('botonbusqueda')
+
+const opciones = document.getElementById('opciones')
+const linkfeed = document.getElementById('linkfeed')
+const linkperfil = document.getElementById('linkperfil')
+const linklogout = document.getElementById('linklogout')
+
+const formaddthingo = document.getElementById('formaddthingo')
+const inputaddnombrethingo = document.getElementById('inputaddnombrethingo')
+const inputaddcategoriathingo = document.getElementById('inputaddcategoriathingo')
+const buttonaddthingo = document.getElementById('buttonaddthingo')
+
+// contenido
+
+const buttonporhacer = document.getElementById('buttonporhacer')
+const dropdownporhacer = document.getElementById('dropdownporhacer')
+
+const buttonfeed = document.getElementById('buttonfeed')
+const dropdownfeed = document.getElementById('dropdownfeed')
+
+const buttonsugerenciasamigos = document.getElementById('buttonsugerenciasamigos')
+const dropdownsugerenciasamigos = document.getElementById('dropdownsugerenciasamigos')
+
+const buttonsugerenciasthingos = document.getElementById('buttonsugerenciasthingos')
+const dropdownsugerenciasthingos = document.getElementById('dropdownsugerenciasthingos')
+
+const buttontrending = document.getElementById('buttontrending')
+const dropdowntrending = document.getElementById('dropdowntrending')
+
+// --------------------
+// Efectos esteticos
+// --------------------
+
+function dropdown(objetivo) {
+    
+}
+
+// --------------------
+// Logica
+// --------------------
