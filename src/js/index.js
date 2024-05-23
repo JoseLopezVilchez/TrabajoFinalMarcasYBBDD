90e4506cdde4718b11afa50172f91de7cfda29ed
JoseLopezVilchez <jose.lopez@escuelaestech.es>
2024-05-23 14:24:31 +0200

    progreso del dia, inicio de las tripas del front

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,25 @@
-const tabregistro = document.getElementById("tabregistro")
-const tablogin = document.getElementById("tablogin")
-const registro = document.getElementById("registro")
-const login = document.getElementById("login")
-const username = document.getElementById("username")
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const repeatpassword = document.getElementById("repeatpassword")
-const usuario = document.getElementById("usuario")
-const contrasenya = document.getElementById("contrasenya")
+import "./index/referencias.js";
+import { toggletablogin , toggletabregistro } from "./index/estetica.js"
+import { loginhandler } from "./index/login.js";
+import { registrohandler } from "./index/registro.js";
+import { username } from "./index/referencias.js";
 
-function hacerprimario(target) {
-    target.classList.remove("border-transparent", "hover:text-gray-600", "hover:border-gray-300", "dark:hover:text-gray-300")
-    target.classList.add("text-blue-600", "border-blue-600", "active", "dark:text-blue-500", "dark:border-blue-500")
-}
+tabregistro.addEventListener('click', () => {
+    toggletabregistro()
+})
 
-function hacersecundario(target) {
-    target.classList.add("border-transparent", "hover:text-gray-600", "hover:border-gray-300", "dark:hover:text-gray-300")
-    target.classList.remove("text-blue-600", "border-blue-600", "active", "dark:text-blue-500", "dark:border-blue-500")
-}
+tablogin.addEventListener('click', () => {
+    toggletablogin()
+})
 
-function toggletablogin() {
-    tabregistro.removeAttribute('aria-current');
-    tablogin.setAttribute('aria-current', 'page');
-    if (login.classList.contains("hidden")) {
-        login.classList.remove("hidden")
-        registro.classList.add("hidden")
-    }
-    hacerprimario(tablogin)
-    hacersecundario(tabregistro)
-}
+login.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-function toggletabregistro() {
-    tablogin.removeAttribute('aria-current');
-    tabregistro.setAttribute('aria-current', 'page');
-    if (registro.classList.contains("hidden")) {
-        registro.classList.remove("hidden")
-        login.classList.add("hidden")
-    }
-    hacerprimario(tabregistro)
-    hacersecundario(tablogin)
-}
+    loginhandler()
+})
+
+registro.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    registrohandler()
+})
---
+++ src/js/index/login.js
@@ -0,0 +1,7 @@
+export function loginhandler() {
+    
+    return {
+        usuario : username
+    }
+
+}
