c5dbe8ed8a87d7ca98d796759041fc089de2428c
JoseLopezVilchez <jose.lopez@escuelaestech.es>
2024-05-31 13:37:08 +0200

    correccion de error en dropdowns

--- src/js/feed.js
+++ src/js/feed.js
@@ -1,48 +1,31 @@
-// --------------------
-// elementos referenciados
-//---------------------
+import "./feed/referencias.js"
+import { toggleDropdown } from "./feed/estetica.js"
+//{ buttonfeed, buttonporhacer, buttonsugerenciasamigos , buttonsugerenciasthingos, buttontrending, dropdownporhacer, dropdownfeed, dropdownsugerenciasamigos, dropdownsugerenciasthingos, dropdowntrending } from 
 
-// drawer
-const formbusqueda = document.getElementById('formbusqueda')
-const inputbusqueda = document.getElementById('inputbusqueda')
-const botonbusqueda = document.getElementById('botonbusqueda')
+document.addEventListener("DOMContentLoaded" , () => {
+    toggleDropdown(dropdownporhacer)
+    toggleDropdown(dropdownfeed)
+    toggleDropdown(dropdownsugerenciasamigos)
+    toggleDropdown(dropdownsugerenciasthingos)
+    toggleDropdown(dropdowntrending)
+})
 
-const opciones = document.getElementById('opciones')
-const linkfeed = document.getElementById('linkfeed')
-const linkperfil = document.getElementById('linkperfil')
-const linklogout = document.getElementById('linklogout')
+buttonporhacer.addEventListener('click', () => {
+    toggleDropdown(dropdownporhacer)
+})
 
-const formaddthingo = document.getElementById('formaddthingo')
-const inputaddnombrethingo = document.getElementById('inputaddnombrethingo')
-const inputaddcategoriathingo = document.getElementById('inputaddcategoriathingo')
-const buttonaddthingo = document.getElementById('buttonaddthingo')
+buttonfeed.addEventListener('click', () => {
+    toggleDropdown(dropdownfeed)
+})
 
-// contenido
+buttonsugerenciasamigos.addEventListener('click', () => {
+    toggleDropdown(dropdownsugerenciasamigos)
+})
 
-const buttonporhacer = document.getElementById('buttonporhacer')
-const dropdownporhacer = document.getElementById('dropdownporhacer')
+buttonsugerenciasthingos.addEventListener('click', () => {
+    toggleDropdown(dropdownsugerenciasthingos)
+})
 
-const buttonfeed = document.getElementById('buttonfeed')
-const dropdownfeed = document.getElementById('dropdownfeed')
-
-const buttonsugerenciasamigos = document.getElementById('buttonsugerenciasamigos')
-const dropdownsugerenciasamigos = document.getElementById('dropdownsugerenciasamigos')
-
-const buttonsugerenciasthingos = document.getElementById('buttonsugerenciasthingos')
-const dropdownsugerenciasthingos = document.getElementById('dropdownsugerenciasthingos')
-
-const buttontrending = document.getElementById('buttontrending')
-const dropdowntrending = document.getElementById('dropdowntrending')
-
-// --------------------
-// Efectos esteticos
-// --------------------
-
-function toggleDropdown(objetivo) {
-    objetivo.classList.toggle('max-h-0')
-}
-
-// --------------------
-// Logica
-// --------------------
-
+buttontrending.addEventListener('click', () => {
+    toggleDropdown(dropdowntrending)
+})
---
+++ src/js/feed/referencias.js
@@ -0,0 +1,35 @@
+// --------------------
+// elementos referenciados
+//---------------------
+
+// drawer
+export const formbusqueda = document.getElementById('formbusqueda')
+export const inputbusqueda = document.getElementById('inputbusqueda')
+export const botonbusqueda = document.getElementById('botonbusqueda')
+
+export const opciones = document.getElementById('opciones')
+export const linkfeed = document.getElementById('linkfeed')
+export const linkperfil = document.getElementById('linkperfil')
+export const linklogout = document.getElementById('linklogout')
+
+export const formaddthingo = document.getElementById('formaddthingo')
+export const inputaddnombrethingo = document.getElementById('inputaddnombrethingo')
+export const inputaddcategoriathingo = document.getElementById('inputaddcategoriathingo')
+export const buttonaddthingo = document.getElementById('buttonaddthingo')
+
+// contenido
+
+export const buttonporhacer = document.getElementById('buttonporhacer')
+export const dropdownporhacer = document.getElementById('dropdownporhacer')
+
+export const buttonfeed = document.getElementById('buttonfeed')
+export const dropdownfeed = document.getElementById('dropdownfeed')
+
+export const buttonsugerenciasamigos = document.getElementById('buttonsugerenciasamigos')
+export const dropdownsugerenciasamigos = document.getElementById('dropdownsugerenciasamigos')
+
+export const buttonsugerenciasthingos = document.getElementById('buttonsugerenciasthingos')
+export const dropdownsugerenciasthingos = document.getElementById('dropdownsugerenciasthingos')
+
+export const buttontrending = document.getElementById('buttontrending')
+export const dropdowntrending = document.getElementById('dropdowntrending')
