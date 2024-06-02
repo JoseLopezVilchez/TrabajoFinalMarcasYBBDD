5f752cbba657b1ead8cb49b2b3f0a4c631c8ba7b
unknown <jose.lopez@escuelaestech.es>
2024-06-02 15:27:45 +0200

    progreso en el login

--- src/js/feed/logica.js
+++ src/js/feed/logica.js
@@ -1,7 +1,3 @@
 function sessionHandler(session_id) {
-    if (sessionStorage.getItem("session_id") != null) {
-
-    } else {
-        sessionStorage.setItem("session_id" , session_id)
-    }
+    
 }
